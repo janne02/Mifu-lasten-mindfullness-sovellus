bfb492e9fca5fca080bce5b7e19183e6a600e083
Janne02 <35040807+janne02@users.noreply.github.com>
2021-05-01 22:37:39 +0300

    Update AboutActivity.java
    Added source

--- src/main/java/com/example/projekti/AboutActivity.java
+++ src/main/java/com/example/projekti/AboutActivity.java
@@ -8,6 +8,7 @@ import android.util.DisplayMetrics;
 import androidx.annotation.Nullable;
 
 /**
+ * idea taken from https://www.youtube.com/watch?v=fn5OlqQuOCk
  * Displays window in MeterActivity with information about project, authors and date.
  */
 public class AboutActivity extends Activity {
@@ -18,11 +19,11 @@ public class AboutActivity extends Activity {
 
         setContentView(R.layout.aboutwindow);
 
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        DisplayMetrics dMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dMetrics);
 
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
+        int width = dMetrics.widthPixels;
+        int height = dMetrics.heightPixels;
 
         getWindow().setLayout((int) (width * .8), (int) (height * .6));
 
